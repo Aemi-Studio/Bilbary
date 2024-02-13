a63e519527f53ec82e06cb3545d4c0f0a4ad6014
Gigi <42325924+g-cqd@users.noreply.github.com>
2024-02-13 11:38:31 +0100

    Initialization Commit

---
+++ iits/iitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iitsApp.swift
+//  iits
+//
+//  Created by Guillaume Coquard on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

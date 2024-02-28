9e654a0ad73529fcb3c59cdcb25a6f327abd44b4
Gigi <42325924+g-cqd@users.noreply.github.com>
2024-02-28 09:27:14 +0100

    Cherry-Pick 1769633 (#5)
    * Cherry-Pick 1769633
    
    * A bit of update
    
    ---------
    
    Co-authored-by: Luca <lucgar@10-20-56-220.0200.fed.it.iosda.org>

--- Bilbary/Views/GenericInfoCard.swift
+++ Bilbary/Views/GenericInfoCard.swift
@@ -14,6 +14,9 @@ struct GenericInfoCard: View {
     var subTitle: String
     var contentText: String
     var nameImage: String
+    var number: Int
+
+    @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -40,16 +43,60 @@ struct GenericInfoCard: View {
         .onTapGesture {
             onTap()
         }
+        customButton(title: title, subtitle: subTitle, contentText: contentText, lineStrokeWidth: 12, numberCircle: number, action: {})
     }
 
+    func customButton(title: String, subtitle: String, contentText: String, lineStrokeWidth: CGFloat, numberCircle: Int, action: @escaping () -> Void) -> some View {
+        Button(action: action) {
+            VStack(alignment: .leading) {
+                HStack {
+
+                    if numberCircle != 0 {
+                        NumberCircle(number: numberCircle, maxNumber: 100, circleSize: 40, strokeWidth: 4, fontSize: 15)
+                    } else {
+                        ZStack {
+                            Rectangle()
+                                .fill(colorScheme == .dark ? Color.gray.opacity(0.07) : Color.white) // Background color
+                                .frame(width: 40, height: 40) // Square dimensions
+
+                            Image(systemName: nameImage)
+                                .foregroundColor(colorScheme == .dark ? .white : .black) // Image color
+                        }
+                        .shadow(radius: 10)
+                    }
+
+                    VStack(alignment: .leading) {
+                        Text(title)
+                            .foregroundColor(colorScheme == .dark ? .white : .black) // Title color
+                            .bold()
+                            .font(.system(size: 25))
+                        Text(subtitle)
+                            .foregroundColor(colorScheme == .dark ? Color.white.opacity(0.6) : Color.black.opacity(0.6)) // Subtitle color
+                            .bold()
+                            .font(.custom("New York", size: 18))
+                    }
+                    Spacer()
+                }
+
+                if contentText != "" {
+                    Text(contentText)
+                        .multilineTextAlignment(.leading)
+                }
+            }
+            .padding(16)
+            .background(.quaternary)
+            .cornerRadius(18)
+        }
+        .buttonStyle(PlainButtonStyle())
+
+    }
 }
 
 // PreviewProvider
 struct GenericInfoCard_Previews: PreviewProvider {
     static var previews: some View {
         GenericInfoCard(onTap: {
-        }, title: "title", subTitle: "subTitle", contentText: "This is a test content", nameImage: "heart")
-        .previewLayout(.sizeThatFits)
-        .background(Color.gray.opacity(0.3))
+        }, title: "title", subTitle: "subTitle", contentText: "This is a test content", nameImage: "heart", number: 0)
+        .background(Color.black)
     }
 }
--- Bilbary/Views/LibraryView.swift
+++ Bilbary/Views/LibraryView.swift
@@ -26,84 +26,91 @@ struct LibraryView: View {
     @State
     private var searchText = ""
 
-    //    @State
-    //    private var searchIsActive = false
+    @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        NavigationStack(path: $view.libraryPath) {
-            ZStack(alignment: .bottom) {
-                VStack(alignment: .leading, spacing: 16) {
+        ZStack {
+            NavigationStack(path: $view.libraryPath) {
+                ZStack(alignment: .bottom) {
+                    VStack(alignment: .leading, spacing: 16) {
 
-                    HStack {
-                        Text("Library")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                        Spacer()
+                        HStack {
+                            Text("Library")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                            Spacer()
 
-                    }
+                        }
 
-                    HStack {
-                        TextField(placeholder, text: $searchText)
-                            .padding(.leading, 30)
-                            .overlay(
-                                HStack {
-                                    Image(systemName: "magnifyingglass")
-                                        .foregroundColor(.gray)
-                                        .padding(.leading, 8)
-                                    Spacer()
-
-                                    if searchText.isEmpty {
-                                        Image(systemName: "mic.fill")
+                        HStack {
+                            TextField("Placeholder", text: $searchText)
+                                .padding(.leading, 30)
+                                .overlay(
+                                    HStack {
+                                        Image(systemName: "magnifyingglass")
                                             .foregroundColor(.gray)
-                                            .padding(.trailing, 8)
+                                            .padding(.leading, 8)
+                                        Spacer()
 
-                                    } else {
-                                        Image(systemName: "xmark.circle.fill")
-                                            .foregroundColor(.gray)
-                                            .padding(.trailing, 8)
-                                            .onTapGesture {
-                                                withAnimation {
-                                                    self.searchText = ""
+                                        if searchText.isEmpty {
+                                            Image(systemName: "mic.fill")
+                                                .foregroundColor(.gray)
+                                                .padding(.trailing, 8)
+                                        } else {
+                                            Image(systemName: "xmark.circle.fill")
+                                                .foregroundColor(.gray)
+                                                .padding(.trailing, 8)
+                                                .onTapGesture {
+                                                    withAnimation {
+                                                        self.searchText = ""
+                                                    }
                                                 }
-                                            }
-
+                                        }
                                     }
-                                }
-                            )
-                            .foregroundColor(.primary)
-                            .padding(10)
-                            .background(Color(.systemGray3))
-                            .cornerRadius(8)
+                                )
+                                .foregroundColor(.primary)
+                                .padding(10)
+                                .background(Color(.systemGray3))
+                                .cornerRadius(8)
+                        }
+                        .padding()
+                        .background(colorScheme == .dark ? Color.black : Color.white)
 
-                    }
-
-                    ScrollView {
-                        VStack(alignment: .leading) {
-                            Text("Today")
-                                .font(.system(size: 14, weight: .semibold))
-
-                            VStack(spacing: 0) {
-                                ForEach(0..<15, id: \.self) { _ in
-                                    LibraryBookCard(onTap: {
-                                        view.libraryPath.append(Book())
-                                    }, number: Int.random(in: 0..<100))
+                        ScrollView {
+                            VStack(alignment: .leading) {
+                                Text("Today")
+                                    .font(.system(size: 14, weight: .semibold))
+                                VStack(spacing: 0) {
+                                    ForEach(0..<15, id: \.self) { _ in
+                                        LibraryBookCard(onTap: {
+                                            view.libraryPath.append(Book())
+                                        }, number: Int.random(in: 0..<100))
+                                        .padding(.all, 7)
+                                    }
                                 }
                             }
                         }
                     }
-                }
-                .padding(0)
-                .safeAreaPadding()
-                .frame(minWidth: 300)
-                .navigationTitle("Library")
-                .navigationBarTitleDisplayMode(.large)
-                .toolbar(.hidden, for: .navigationBar)
-                .navigationDestination(for: Book.self, destination: BookInformationView.init)
+                    .padding(0)
+                    .safeAreaPadding()
+                    .frame(minWidth: 300)
+                    .navigationTitle("Library")
+                    .navigationBarTitleDisplayMode(.large)
+                    .toolbar(.hidden, for: .navigationBar)
+                    .navigationDestination(for: Book.self, destination: BookInformationView.init)
 
-                CustomHStack(viewOption: $viewOption)
+                    CustomHStack(viewOption: $viewOption)
+                }
+                /*
+                 .overlay(LinearGradient(
+                 gradient: Gradient(colors: [.clear, .white]),
+                 startPoint: .top,
+                 endPoint: .bottom
+                 ))
+                 */
             }
+            .searchable(text: $searchText)
         }
-        .searchable(text: $searchText)
     }
 }
 

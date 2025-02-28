65548987673102eeff81ba5f672afaf5fe6c5719
Shohjakhon Mamadaliev <shohjakhon.mamadaliev@ipratico.it>
2025-02-28 17:10:28 +0100

    Created new CustomizationView for iPhone reusing old one for iPad

--- Bilbary for iPhone/Views/CustomizationView.swift
+++ Bilbary for iPhone/Views/CustomizationView.swift
@@ -4,28 +4,28 @@
 //
 //  Created by Shohjakhon Mamadaliev on 28/02/25.
 //
-
-import SwiftUI
-
-struct CustomizationView: View {
-    var body: some View {
-        VStack(spacing: 16) {
-            VStack {
-                Text("Customization View")
-            }
-            .frame(maxWidth: .infinity)
-            .frame(height: 200)
-            .background(.bar)
-            .clipShape(RoundedRectangle(cornerRadius: 8))
-            .padding()
-
-            ScrollView {
-
-            }
-        }
-    }
-}
-
-#Preview {
-    CustomizationView()
-}
+//
+//import SwiftUI
+//
+//struct CustomizationView: View {
+//    var body: some View {
+//        VStack(spacing: 16) {
+//            VStack {
+//                Text("Customization View")
+//            }
+//            .frame(maxWidth: .infinity)
+//            .frame(height: 200)
+//            .background(.bar)
+//            .clipShape(RoundedRectangle(cornerRadius: 8))
+//            .padding()
+//
+//            ScrollView {
+//
+//            }
+//        }
+//    }
+//}
+//
+//#Preview {
+//    CustomizationView()
+//}
